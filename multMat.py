2cabad3e6d4beacb4eb9a5d1694ba94784ae62e0
Joan Merce Lopez <joan.merce@estudiants.urv.cat>
2020-03-23 15:49:14 +0100

    No funciona multiplicar
    Crec que no agafa be les matrius amb el get_object

--- multMat.py
+++ multMat.py
@@ -10,9 +10,9 @@ numWorkers = 1
 matriuA = []
 matriuB = []
 matriuC = []
-m = 10           # files de la matriu A
-n = 10           # columnes de la matriu A // files de la matriu B
-l = 10           # columnes de la matriu B
+m = 2           # files de la matriu A
+n = 2           # columnes de la matriu A // files de la matriu B
+l = 2           # columnes de la matriu B
 
 
 
@@ -21,38 +21,62 @@ def inicialitzar(bucket, ibm_cos):
     numFiles = 0
     files = []
     for i in range(numWorkers):
-        if (i < numWorkers - 1):
+        if (i < numWorkers - 1):            # trobar quantes columnes crear
             rang = int(m/numWorkers)
         else:
             rang = m - numFiles
 
-        for j in range(rang):
+        for j in range(rang):               # crear files
             np.append(files, np.random.randint(10, size=n))
             numFiles += 1
 
-        partSer = pickle.dumps(files, pickle.HIGHEST_PROTOCOL)
-        ibm_cos.put_object(Bucket=bucket, Key="A"+str(i), Body=partSer)
+        partSer = pickle.dumps(files, pickle.HIGHEST_PROTOCOL)          # serialitzar i guardar
+        ibm_cos.put_object(Bucket=bucket, Key='A'+str(i), Body=partSer)
     
     # Inicialitzar i guardar matriu B
     numCol = 0
     col = []
     for i in range(numWorkers):
-        if (i < numWorkers - 1):
+        if (i < numWorkers - 1):            # trobar quantes columnes crear
             rang = int(l/numWorkers)
         else:
             rang = l - numCol
 
-        for j in range(rang):
+        for j in range(rang):               # crear columnes 
             np.append(col, np.random.randint(10, size=n))
             numCol += 1
 
-        partSer = pickle.dumps(col, pickle.HIGHEST_PROTOCOL)
-        ibm_cos.put_object(Bucket=bucket, Key="B"+str(i), Body=partSer)
+        partSer = pickle.dumps(col, pickle.HIGHEST_PROTOCOL)            # serialitzar i guardar
+        ibm_cos.put_object(Bucket=bucket, Key='B'+str(i), Body=partSer)
 
     
 
-#def multiplicar(bucket, ibm_cos):
-# deserialitzar --> part = pickle.loads(get)
+def multiplicar(bucket, ibm_cos, id):
+    # agafar chunk Aid i Bid
+    chunkASer = ibm_cos.get_object(Bucket=bucket, Key='A'+str(id))['Body'].read()
+    chunkBSer = ibm_cos.get_object(Bucket=bucket, Key='B'+str(id))['Body'].read()
+
+    # desserialitzar chunks
+    chunkA = pickle.loads(chunkASer)
+    print(chunkA)
+    chunkB = pickle.loads(chunkBSer)
+    print(chunkB)
+
+    # multiplicar chunks
+    chunkC = []
+    for i in range(int(len(chunkA)/n)):
+        for j in range(int(len(chunkB)/n)):
+            suma = np.zeros(1, dtype=int)
+            for k in range(n):
+                print(k)
+                suma += chunkA[i][k] * chunkB[j][k]
+            np.append(chunkC, np.array(suma))  
+
+    partSer = pickle.dumps(chunkC, pickle.HIGHEST_PROTOCOL)            # serialitzar i guardar
+    ibm_cos.put_object(Bucket=bucket, Key='C'+str(id)+str(id), Body=partSer)
+          
+
+
 
 #def reduir():
 
@@ -63,14 +87,12 @@ if __name__ == '__main__':
         if numWorkers <= m*n:
             ibmcf = pywren.ibm_cf_executor()
             ibmcf.call_async(inicialitzar, 'sd-python')
-            '''
+            interdata = ['sd-python']
             if(numWorkers == 1):
-                interdata = []
                 ibmcf.map(multiplicar, interdata)
             else:
-                interdata = []
                 ibmcf.map_reduce(multiplicar, interdata, reduir)
-            '''
+
             print(ibmcf.get_result())
             
             ibmcf.clean()

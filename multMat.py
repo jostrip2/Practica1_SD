ffc02215af1063f147f269c67393ef1b05b6e774
Joan Merce Lopez <joan.merce@estudiants.urv.cat>
2020-03-24 20:20:56 +0100

    Acabat multiplicar
    Crec que no esta be la funcio per dividir la matriu. A l'enunciat nse quin es el valor de 'a'. Crec q es per aixo

--- multMat.py
+++ multMat.py
@@ -1,12 +1,11 @@
 import pywren_ibm_cloud as pywren
-import numpy as np
+#import numpy as np
 import sys
 import pickle
-import marshal
+import random
 
 MAX_WORKERS = 100   # numero de workers maxim
 
-numWorkers = 1
 
 m = 2           # files de la matriu A
 n = 2           # columnes de la matriu A // files de la matriu B
@@ -14,98 +13,102 @@ l = 2           # columnes de la matriu B
 
 
 
-def inicialitzar(bucket, ibm_cos):
+def inicialitzar(bucket, workers, ibm_cos):
     # Inicialitzar i guardar matriu A
     numFiles = 0
-    files = []
-    for i in range(numWorkers):
-        if (i < numWorkers - 1):            # trobar quantes columnes crear
-            rang = int(m/numWorkers)
+    for i in range(workers):
+        if (i < workers - 1):            # trobar quantes columnes crear
+            rang = int(m/workers)
         else:
             rang = m - numFiles
                
-        files = np.append(files, np.random.randint(10, size=(rang, n)))     # crear files
+        matA = []
+        for j in range(rang):                  # crear files
+            matA.append([])
+            for _ in range(n):
+                matA[j].append(random.randint(0, 10))
+
         numFiles += rang
 
-        partSer = marshal.dumps(files.astype(int))
-        #partSer = pickle.dumps(files, pickle.HIGHEST_PROTOCOL)          # serialitzar i guardar
+        partSer = pickle.dumps(matA)          # serialitzar i guardar
         ibm_cos.put_object(Bucket=bucket, Key='A'+str(i), Body=partSer)
     
     # Inicialitzar i guardar matriu B
     numCol = 0
-    col = []
-    for i in range(numWorkers):
-        if (i < numWorkers - 1):            # trobar quantes columnes crear
-            rang = int(l/numWorkers)
+    for i in range(workers):
+        if (i < workers - 1):            # trobar quantes columnes crear
+            rang = int(l/workers)
         else:
             rang = l - numCol
-             
-        col = np.append(col, np.random.randint(10, size=(rang,n)))         # crear columnes 
+                     
+        matB = []
+        for j in range(n):                   # crear columnes
+            matB.append([])
+            for _ in range(rang):
+                matB[j].append(random.randint(0, 10)) 
         numCol += rang
 
-        partSer = marshal.dumps(col.astype(int))
-        #partSer = pickle.dumps(col, pickle.HIGHEST_PROTOCOL)            # serialitzar i guardar
+        partSer = pickle.dumps(matB)            # serialitzar i guardar
         ibm_cos.put_object(Bucket=bucket, Key='B'+str(i), Body=partSer)
 
-    return [files.astype(int), col.astype(int)]
+    return [matA, matB]
 
     
 
-def multiplicar(bucket, ibm_cos, id):
+def multiplicar(num, bucket, ibm_cos):
     # agafar chunk Aid i Bid
-    chunkASer = ibm_cos.get_object(Bucket=bucket, Key='A'+str(id))['Body'].read()
-    chunkBSer = ibm_cos.get_object(Bucket=bucket, Key='B'+str(id))['Body'].read()
+    chunkASer = ibm_cos.get_object(Bucket=bucket, Key='A'+str(num))['Body'].read()
+    chunkBSer = ibm_cos.get_object(Bucket=bucket, Key='B'+str(num))['Body'].read()
 
     # desserialitzar chunks
     chunkA = pickle.loads(chunkASer)
-    print(chunkA)
     chunkB = pickle.loads(chunkBSer)
-    print(chunkB)
 
     # multiplicar chunks
     chunkC = []
-    for i in range(int(len(chunkA)/n)):
-        for j in range(int(len(chunkB)/n)):
-            suma = np.zeros(1, dtype=int)
-            for k in range(n):
-                print(k)
-                suma += chunkA[i][k] * chunkB[j][k]
-            np.append(chunkC, np.array(suma))  
+    i = 0
+    for fila in chunkA:
+        for col in chunkB:
+            suma = 0
+            for i in range(len(col)):
+                suma += fila[i] * col[i] 
+            chunkC.append(suma)
+        i += 1  
 
-    partSer = pickle.dumps(chunkC, pickle.HIGHEST_PROTOCOL)            # serialitzar i guardar
-    ibm_cos.put_object(Bucket=bucket, Key='C'+str(id)+str(id), Body=partSer)
+    partSer = pickle.dumps(chunkC)            # serialitzar i guardar
+    ibm_cos.put_object(Bucket=bucket, Key='C'+str(num)+str(num), Body=partSer)
+    return [chunkC]
           
-
-
 
 #def reduir():
 
 
 def test(bucket, ibm_cos):
-    chunkASer = ibm_cos.get_object(Bucket=bucket, Key='A0')['Body'].read()
-    chunkBSer = ibm_cos.get_object(Bucket=bucket, Key='B0')['Body'].read()
-    chunkA = marshal.loads(chunkASer)
-    chunkB = marshal.loads(chunkBSer)
-    print(str(chunkA)+'\t'+str(chunkB))
-    #return [chunkA, chunkB]
+    chunkCSer = ibm_cos.get_object(Bucket=bucket, Key='C00')['Body'].read()
+    chunkC = pickle.loads(chunkCSer)
+    return chunkC 
 
 
 if __name__ == '__main__':
     try:
         numWorkers = int(sys.argv[1])
+    except IndexError:
+        print("S'ha d'indicar el nombre de workers")
+    else:
         if numWorkers > 0:
             if numWorkers <= MAX_WORKERS:
                 if numWorkers <= m*n:
                     ibmcf = pywren.ibm_cf_executor()
-                    ibmcf.call_async(inicialitzar, 'sd-python')
-                    interdata = ['sd-python']
-                    ibmcf.call_async(test, 'sd-python')
-                    '''
+                    params = {'bucket': 'sd-python', 'workers': numWorkers}
+                    ibmcf.call_async(inicialitzar, params)
+                    interdata = list(range(numWorkers))
                     if(numWorkers == 1):
-                        ibmcf.map(multiplicar, interdata)
+                        ibmcf.map(multiplicar, interdata, extra_params=['sd-python'])
+                    '''
                     else:
                         ibmcf.map_reduce(multiplicar, interdata, reduir)
                     '''
+                    #ibmcf.call_async(test, 'sd-python')
                     print(ibmcf.get_result())
                     
                     ibmcf.clean()
@@ -115,8 +118,7 @@ if __name__ == '__main__':
                 print("El nombre de workers ha de ser inferior a 100")
         else:
             print("El nombre de workers ha de ser superior a 0")
-    except IndexError:
-        print("S'ha d'indicar el nombre de workers")
+    
     
     
     
